1f834f4f475bdf7d386fedb17a251e5711eed3e2
shrikant-nidasheshi <shreekantnidasheshi@gmail.com>
2026-03-17 20:03:20 +0530

    added correct URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 // Axios instance - attaches JWT token to every request automatically
 import axios from "axios";
 
-const API_BASE_URL = "https://student-management-backend-dief.onrender.com";
+const API_BASE_URL = "https://student-management-backend-dief.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
